009a9a9e53fe93a5247cc88655b4b1e5bd6d9673
gabriel <dgabrielfof@gmail.com>
2024-07-12 11:37:24 -0300

    :sparkles: feat: structuring the product detail area

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -61,23 +61,100 @@ Card.create = (item) => {
   detailButton.textContent = "detalhes";
   buttonArea.appendChild(detailButton);
 
-  const detailArea = document.createElement("div");
-  detailArea.classList.add("detail_area");
+  const detailArea = createDetailArea(item);  
   card.appendChild(detailArea);
 
-  const detail = {};
-
-  detail.name = document.createElement("span");
-  detail.name.classList.add("name");
-  detail.name.textContent = item.name;
-
-  if (item.details) {
-    for (const detail in item.details) {
-      // console.log(detail)
-      
-    }
-  }
   return card;
+}
+
+function createDetailArea(item) {
+  const detailArea = document.createElement("div");
+  detailArea.classList.add("detail_area");
+  detailArea.classList.add("hide");
+
+  const imagesArea = document.createElement("div");
+  imagesArea.classList.add("images_area");
+  detailArea.appendChild(imagesArea);
+
+  const imagesMenu = document.createElement("div");
+  imagesMenu.classList.add("image_menu");
+  imagesArea.appendChild(imagesMenu);
+
+  const imagesDetail = document.createElement("div");
+  imagesDetail.classList.add("image_detail");
+  imagesArea.appendChild(imagesDetail);
+
+  const details = document.createElement("div");
+  details.classList.add("details");
+  detailArea.appendChild(details);
+
+  const name = document.createElement("span");
+  name.classList.add("name");
+  name.textContent = item.name;
+  details.appendChild(name);
+
+  const description = document.createElement("p");
+  description.classList.add("description");
+  description.textContent = item.description;
+  details.appendChild(description);
+
+  const classification = document.createElement("div");
+  classification.classList.add("classification");
+  details.appendChild(classification);
+
+  const category = document.createElement("span");
+  category.textContent = item.category;
+  classification.appendChild(category);
+  
+  const arrow = document.createElement("img");
+  arrow.src = "src/assets/icons/arrow.svg"
+  classification.appendChild(arrow);
+
+  const type = document.createElement("span");
+  type.textContent = item.type;
+  classification.appendChild(type);
+
+  const other = document.createElement("div");
+  other.classList.add("other");
+  details.appendChild(other);
+
+  const options = document.createElement("div");
+  options.classList.add("options");
+  other.appendChild(options);
+
+  const availableSizes = document.createElement("div");
+  availableSizes.classList.add("available_sizes");
+  options.appendChild(availableSizes);
+
+  const models = document.createElement("div");
+  models.classList.add("models");
+  options.appendChild(models);
+
+  const physicalProperties = document.createElement("div");
+  physicalProperties.classList.add("physical_properties");
+  other.appendChild(physicalProperties);
+
+  const height = document.createElement("span");
+  height.classList.add("height");
+  physicalProperties.appendChild(height);
+
+  const width = document.createElement("span");
+  width.classList.add("width");
+  physicalProperties.appendChild(width);
+
+  const depth = document.createElement("span");
+  depth.classList.add("depth");
+  physicalProperties.appendChild(depth);
+
+  const weight = document.createElement("span");
+  weight.classList.add("weight");
+  physicalProperties.appendChild(weight);
+
+  const volume = document.createElement("span");
+  volume.classList.add("volume");
+  physicalProperties.appendChild(volume);
+
+  return detailArea;
 }
 
 function buy(event, link) {
@@ -86,7 +163,13 @@ function buy(event, link) {
 }
 
 function toggleDetail(event) {
-  console.log(event.target, "detalhes")
+  const parentCard = event.target.closest(".card");
+  const detailArea = parentCard.querySelector(".detail_area")
+  if (detailArea.classList.contains("hide")) {
+    detailArea.classList.remove("hide");
+  } else {
+    detailArea.classList.add("hide");
+  }
 }
 
 Card.remove = async (product) => {
